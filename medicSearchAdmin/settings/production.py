7888fea8b76c5dd30dd07fd70811aad03f3fe54a
valdeirfilho <valdeirfilho@msn.com>
2022-04-01 20:59:27 -0300

    book_django_A_Z

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,16 @@
+import os
+from django.template import Engine
+from .settings import *
+
+DEBUG = False
+
+SECRET_KEY = 'django-insecure-f5-pf*wb$xqo35#krp%4qs(a^h5a^-c8gw=b_g+5qf3)p*cul2'
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join (BASE_DIR, 'db.sqlite3')
+    }
+}
